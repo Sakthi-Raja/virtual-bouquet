e2a17181a04597228b7db9625677e623d3abf982
Sakthi <sakthiraja095@gmail.com>
2026-01-28 22:37:03 +0530

    view page fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Create from "./pages/create";
 import View from "./pages/View";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Create />} />
-        <Route path="/view" element={<View />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Create />} />
+      <Route path="/view" element={<View />} />
+    </Routes>
   );
 }
 
